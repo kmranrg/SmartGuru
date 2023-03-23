4496300bb542bbad43c21c5257a8bbef3dbf61c9
Kumar Anurag <kan@Kumars-MacBook-Air.local>
2023-03-23 22:12:48 +0530

    chat-assistant integration done

--- assistant.py
+++ assistant.py
@@ -2,31 +2,34 @@ import openai
 
 openai.api_key = str(open("token.txt").read())
 
-messages = [
-    {"role": "system", "content": "You are a helpful assistant."},
-]
+class SmartGurucool():
 
-status = True
+    def __init__(self):
+        self.messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+        ]
 
-while status != False:
-  user_question = input("What do you wanna ask?")
+    def SmartGuruResponse(self, user_text):
+        self.user_text = user_text
 
-  # if user says stop, then breaking the loop
-  if user_question == "stop":
-    break
-  
-  # storing the user question in the messages list
-  messages.append({"role": "user", "content": user_question})
+        while True:
 
-  # getting the response from OpenAI API
-  response= openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=messages
-  )
+            # if user says stop, then breaking the loop
+            if self.user_text == "stop":
+                break
+            
+            # storing the user question in the messages list
+            self.messages.append({"role": "user", "content": self.user_text})
 
-  # appending the generated response so that AI remebers past responses
-  messages.append({"role":"assistant", "content":str(response['choices'][0]['message']['content'])})
-  print(response['choices'][0]['message']['content'])
+            # getting the response from OpenAI API
+            response= openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=self.messages
+            )
 
-for i in messages:
-  print(i)
+            # appending the generated response so that AI remebers past responses
+            self.messages.append({"role":"assistant", "content":str(response['choices'][0]['message']['content'])})
+            
+            # returning the response
+            print(response['choices'][0]['message']['content'])
+            return response['choices'][0]['message']['content']
